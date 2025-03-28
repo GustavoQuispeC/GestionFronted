3ca0b98ee6e56b1904d9eafeb3348e00349a95f8
Gustavo <gusstavocta@gmail.com>
2025-03-27 23:24:05 -0500

    implementando navbar

---
+++ components/carousel/HeroWithCarousel.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Image from "next/image";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const images = [
+  { src: "/images/hero1.jpg", alt: "Hero Image 1" },
+  { src: "/images/hero2.jpg", alt: "Hero Image 2" },
+  { src: "/images/hero3.jpg", alt: "Hero Image 3" },
+];
+
+export default function HeroWithCarousel() {
+  return (
+    <section className="relative w-full h-[500px] bg-gray-800 flex justify-center items-center">
+      <Carousel
+        showArrows={true}
+        autoPlay
+        infiniteLoop
+        showThumbs={false}
+        showStatus={false}
+        className="w-full max-w-[1920px]"
+      >
+        {images.map((image, index) => (
+          <div key={index} className="relative w-full h-[500px]">
+            <Image
+              src={image.src}
+              alt={image.alt}
+              width={1920}
+              height={500}
+              objectFit="cover"
+              className="rounded-lg shadow-lg"
+            />
+          </div>
+        ))}
+      </Carousel>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+        <h1 className="text-4xl font-bold">Bienvenido a nuestra tienda</h1>
+        <p className="mt-2 text-lg">Descubre lo mejor en nuestros productos</p>
+      </div>
+    </section>
+  );
+}
---
+++ components/index.tsx
@@ -0,0 +1,3 @@
+export { default as HeroWithCarousel } from "./carousel/HeroWithCarousel";
+export { default as ComplexNavbar } from "./navbar/ComplexNavbar";
+export { default as Footer } from "./footer/Footer";
